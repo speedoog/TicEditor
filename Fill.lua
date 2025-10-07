fd2064533e6de787ef96fa6cb0f217992a73b04a
Speedman <mail.bertrand.faure@gmail.com>
2025-10-07 22:59:54 +0200

    cleanup stuff

--- Fill.lua
+++ Fill.lua
@@ -1,36 +1,35 @@
 
 function FloodFill(x, y, c)
-    if not InScreen(x, y) then return end
+	if not InScreen(x, y) then return end
 
-    loops = 0
+	loops = 0
 
-    local o = pix(x, y)
-    local q = CreateQueue()
-    q:push({x, y})
-    while q:isEmpty() == false do
-        loops = loops + 1
-        if loops / 2 > t % 200 then return end
+	local o = pix(x, y)
+	local q = CreateQueue()
+	q:push({x, y})
+	while q:isEmpty() == false do
+		loops = loops + 1
+		if loops / 2 > t % 200 then return end
 
-        local v = q:pop()
-        x = v[1]
-        y = v[2]
+		local v = q:pop()
+		x = v[1]
+		y = v[2]
 
-        local lx = x
+		local lx = x
 
-        while Inside(lx - 1, y, o) do
-            pix(lx - 1, y, c)
-            lx = lx - 1
-        end
+		while Inside(lx - 1, y, o) do
+			pix(lx - 1, y, c)
+			lx = lx - 1
+		end
 
-        while Inside(x, y, o) do
-            pix(x, y, c)
-            x = x + 1
-        end
+		while Inside(x, y, o) do
+			pix(x, y, c)
+			x = x + 1
+		end
 
-        scan(q, lx, x - 1, y + 1, o)
-        scan(q, lx, x - 1, y - 1, o)
-    end
-
+		scan(q, lx, x - 1, y + 1, o)
+		scan(q, lx, x - 1, y - 1, o)
+	end
 end
 
 function InScreen(x, y) return x >= 0 and x < 240 and y >= 0 and y < 136 end
@@ -38,16 +37,16 @@ function InScreen(x, y) return x >= 0 and x < 240 and y >= 0 and y < 136 end
 function Inside(x, y, o) return pix(x, y) == o and InScreen(x, y) end
 
 function scan(q, lx, rx, y, o)
-    local span_added = false
-    local x
-    for x = lx, rx do
-        if Inside(x, y, o) == false then
-            span_added = false
-        elseif not span_added then
-            q:push({x, y})
-            span_added = true
-        end
-    end
+	local span_added = false
+	local x
+	for x = lx, rx do
+		if Inside(x, y, o) == false then
+			span_added = false
+		elseif not span_added then
+			q:push({x, y})
+			span_added = true
+		end
+	end
 end
 
 --[[
